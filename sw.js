31bee2d413e8794fe7999543752601aa21e9d061
chandanpallar <chandanpallarwhat@gmail.com>
2026-08-27 16:48:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,42 @@
+importScripts(
+    "https://www.gstatic.com/firebasejs/12.18.0/firebase-app-compat.js"
+);
+
+importScripts(
+    "https://www.gstatic.com/firebasejs/12.18.0/firebase-messaging-compat.js"
+);
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCpip3JYy6CDpStzSo5Ak8hTRR9CTv72Uw",
+    authDomain: "studentmart-726f4.firebaseapp.com",
+    projectId: "studentmart-726f4",
+    storageBucket: "studentmart-726f4.firebasestorage.app",
+    messagingSenderId: "441914681487",
+    appId: "1:441914681487:web:49f8e75d2603e6004328fa"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+    console.log(
+        "Background message received:",
+        payload
+    );
+
+    const notification =
+        payload.notification || {};
+
+    self.registration.showNotification(
+        notification.title || "StudentMart",
+        {
+            body:
+                notification.body ||
+                "You have a new message.",
+            icon: "/Student-Mart/icon-192.png"
+        }
+    );
+});
+
 self.addEventListener("install", event => {
     self.skipWaiting();
 });
